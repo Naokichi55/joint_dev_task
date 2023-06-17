e45158a433a6fce41c97d5c86db9346c22c2b7c6
Naokichi55 <ibosanganba@gmail.com>
2023-06-17 16:20:46 +0900

    Ruby(基礎) Q14,Q15,Q16を解答しました。

--- task.rb
+++ task.rb
@@ -149,8 +149,13 @@ def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-  
-
+  #空の配列の作成
+    data1 = []
+  #キーを配列にいれる
+  data.each_key do |key|
+    data1 << key
+  end
+    p data1
 end
 
 def q15
@@ -158,7 +163,17 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  if data1.include?(:age)
+    puts "OK"
+  else
+    puts "NG"
+  end
+  
+  if data2.include?(:age)
+    puts "OK"
+  else
+    puts "NG"
+  end
 end
 
 def q16
@@ -170,7 +185,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do |user|
+      puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
+  end
 end
 
 class UserQ17
